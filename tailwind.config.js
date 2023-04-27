bf33d9aa82b20b0a7b8beac1a403830f159d9b90
Roy-de <roymuuri28@gmail.com>
2023-04-27 15:29:02 +0300

    today

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-        extend: {},
+        screens:{
+            'sm': '6400px',
+            'md': '768px',
+            'lg': '1024px',
+            'xl':'1280px',
+            '2xl': '1536px',
+        },
+        borderRadius:{
+            'none': '0',
+            DEFAULT: '0.25rem',
+            'large': '12px',
+        },
+        extend: {
+            spacing: {
+                '128': '32rem',
+                '144': '36rem',
+            },
+            borderRadius: {
+                '4xl': '2rem',
+            }
+        },
     },
     plugins: [],
     }
